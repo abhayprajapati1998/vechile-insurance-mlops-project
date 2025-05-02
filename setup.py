069891ca67aa3396e0d027b85e5d5e6566ff3820
Abhay <abhay.pra.s@gmail.com>
2025-05-02 07:59:15 +0530

    project structure is ready , my_env is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhay Prajapati",
+    author_email="abhay.pra.s@gmail.com",
+    packages=find_packages()
+)
